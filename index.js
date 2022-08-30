23422a4e273dc2af79065269d1e78bd0f73360fc
Emmanuel <emmanue.kimweli@student.moringaschool.com>
2022-08-30 13:59:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+// const dodger = document.getElementById("dodger");dodger.style.backgroundColor = "#FF69B4";
+
+
+// dodger.style.backgroundColor = "#FF69B4";
+
+// dodger.style.bottom = "100px";
+
+// dodger.style.bottom = "0px";
+
+// dodger.style.left = "0px";
+
+
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  else{
+        moveDodgerRight();
+     }
+    }
+  );
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left + 1}px`;
+  }
+
